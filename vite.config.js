dfc37fef8b093ddc5eb598a080cf62f7024f56fe
Serge <smdyachenko@gmail.com>
2023-05-09 06:19:24 +0300

    Change dist directory

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../dist'
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
